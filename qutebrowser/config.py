055c9a5d5803f2d7bfaee18e2647d1ec79522f14
ilasie <ilasie@163.com>
2026-02-20 21:55:10 +0800

    add qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,26 @@
+config.source('gruvbox.py')
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.preferred_color_scheme = "dark"
+c.colors.webpage.darkmode.policy.images = "never"
+
+config.source('bindings.py')
+
+config.load_autoconfig(False)
+
+c.url.start_pages = 'about:blank'
+c.url.default_page = 'about:blank'
+c.tabs.last_close = "startpage"
+c.url.searchengines = {
+    "DEFAULT": "https://cn.bing.com/search?q={}"
+}
+
+c.statusbar.show = "always"
+
+c.fonts.default_size = '14pt'
+c.fonts.hints = 'normal 12pt default_family'
+
+c.tabs.show = "always"
+c.tabs.position = "left"
+c.tabs.padding = {"bottom":0, "left":0, "right":0, "top":0}
+c.tabs.indicator.width = 0
+c.tabs.width = '10%'
