f5a196e9452670e1e241ad90068230f4078b31ae
ilasie <ilasie@163.com>
2026-03-13 12:44:25 +0800

    update qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import shlex
+
 config.source('gruvbox.py')
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = "dark"
@@ -6,6 +8,15 @@ c.colors.webpage.darkmode.policy.images = "never"
 config.source('bindings.py')
 
 config.load_autoconfig(False)
+
+term = 'alacritty -e'
+
+# required to create link `/bin/yazi` if it had not been there
+fileChooser = shlex.split(term) + ['yazi', '--chooser-file={}']
+c.fileselect.handler = "external"
+c.fileselect.folder.command = fileChooser
+c.fileselect.multiple_files.command = fileChooser
+c.fileselect.single_file.command = fileChooser
 
 c.url.start_pages = 'about:blank'
 c.url.default_page = 'about:blank'
@@ -24,3 +35,6 @@ c.tabs.position = "left"
 c.tabs.padding = {"bottom":0, "left":0, "right":0, "top":0}
 c.tabs.indicator.width = 0
 c.tabs.width = '10%'
+
+c.downloads.remove_finished = 3300
+c.downloads.position = "bottom"
